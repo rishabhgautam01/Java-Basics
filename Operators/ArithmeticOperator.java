3dc684f53f16aa5bae3465b4098977bf58603039
RISHABH GAUTAM <rishabhgautam0000@gmail.com>
2021-05-28 02:18:34 +0530

    Type 1) Arithmetic Operators

---
+++ Operators/ArithmeticOperator.java
@@ -0,0 +1,31 @@
+public class ArithmeticOperator {
+
+	public static void main(String[] args) {
+			
+		int A = 10;
+		int B = 20;
+		
+		// Addition
+		int add = A + B;
+		System.out.println("Addition  : "  + add);
+		
+		// Subtraction
+		System.out.println("Sub  : "  + (A-B));
+		
+		// Multiply
+		int mul = A * B ;
+		System.out.println("Mul  : "  + mul);
+		
+		//Division
+		int div = A/B;
+		System.out.println("Div  : "  + div);
+		
+		// Modulus %
+		int mod = A % B;
+		System.out.println("Mod  : "  + mod);
+		
+		
+
+	}
+
+}
